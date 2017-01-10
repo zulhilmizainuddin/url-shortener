56a573f6a0fcc5e95bc53726fce3c079c3944e39
Zulhilmi Mohd Zainuddin <zulhilmi@ascom.com>
2017-01-10 13:29:02 +0800

    Add sql query wrapper

--- models/database-query.js
+++ models/database-query.js
@@ -8,14 +8,59 @@ class DatabaseQuery {
 
         this.db = new sqlite3.Database(shortenerDb);
         this.db.serialize(() => {
-            this.db.run('CREATE TABLE if not exists url (id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT, key INTEGER)');
+            this.db.run(
+                `CREATE TABLE if not exists url(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                original_url TEXT,
+                key INTEGER)`
+                );
         });
     }
 
-    insertUrl(url) {
+    insertOriginalUrl(url) {
         this.db.serialize(() => {
             this.db.run('INSERT INTO url VALUES (?, ?, ?)', [null, url, -1]);
         });
+    }
+
+    queryId(url) {
+
+        return new Promise((resolve, reject) => {
+            this.db.serialize(() => {
+                this.db.get(`SELECT id FROM url WHERE original_url='${url}'`, (err, row) => {
+                    if (!err) {
+                        resolve(row.id);
+                    } else {
+                        reject(err);
+                    }
+                });
+            });
+        });
+    }
+
+    queryOriginalUrl(key) {
+
+        return new Promise((resolve, reject) => {
+            this.db.serialize(() => {
+                this.db.get(`SELECT original_url FROM url WHERE key='${key}'`, (err, row) => {
+                    if (!err) {
+                        resolve(row.id);
+                    } else {
+                        reject(err);
+                    }
+                });
+            });
+        });
+    }
+
+    updateKey(id, key) {
+        this.db.serialize(() => {
+            this.db.run('UPDATE url SET key=? WHERE id=?', [key, id]);
+        });
+    }
+
+    close() {
+        this.db.close();
     }
 }
 
