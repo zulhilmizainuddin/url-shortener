3d962e7bacd9ec17b18b93c9268333c4c785c02e
Zulhilmi Mohd Zainuddin <zulhilmi@ascom.com>
2017-01-10 13:30:05 +0800

    Use DatabaseQuery in shorten route. Partial implementation

--- routes/shorten.js
+++ routes/shorten.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
+const IdToKeyConverter = require('../utils/id-to-key-converter');
+
 const HttpStatus = require('http-status-codes');
 const DatabaseQuery = require('../models/database-query');
 
@@ -10,9 +12,23 @@ router.get('/:id', (req, res, next) => {
 
     const url = req.params.id;
     const databaseQuery = new DatabaseQuery();
-    databaseQuery.insertUrl(url);
 
-    res.status(HttpStatus.OK).send({  });
+    databaseQuery.insertOriginalUrl(url);
+    databaseQuery
+        .queryId(url)
+        .then((id) => {
+            const key = IdToKeyConverter.convertIdToKey(id);
+
+            databaseQuery.updateKey(id, key);
+            
+            res.status(HttpStatus.OK).send({
+                id: id,
+                key: key
+            });
+        })
+        .catch((err) => {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ error_message: err });
+        });
 });
 
 module.exports = router;
