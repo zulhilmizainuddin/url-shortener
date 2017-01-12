8faf6dc818183209aa3e255255386991a2905bea
Zulhilmi Mohamed Zainuddin <hilmi343gs@gmail.com>
2017-01-12 14:07:58 +0800

    Shorten route unit test

---
+++ test/shorten-route-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('request');
+
+const UrlValidation = require('../utils/url-validation');
+
+const config = require('../config');
+
+describe('Shorten route', () => {
+    it('shortened url', (done) => {
+        request.post(`http://${config.base_url}/shorten`, {form: {url: 'http://github.com'}}, (err, res, body) => {
+            const result = JSON.parse(body);
+
+            const isSupportedUrl = UrlValidation.isSupportedUrl(result.shortened_url);
+
+            expect(isSupportedUrl).to.be.true;
+            done();
+        });
+    });
+});
