c0e010e7acb540cd0ce9d2e6c18db25d70f89447
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2017-01-12 18:35:22 +0800

    Expand route unit test

---
+++ test/expand-route-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('request');
+
+const UrlValidation = require('../utils/url-validation');
+
+const config = require('../config');
+
+describe('Expand route', () => {
+    it('expanded url', (done) => {
+        request.post(`http://${config.base_url}/expand`, {form: {url: `http://${config.base_url}/1`}}, (err, res, body) => {
+            const result = JSON.parse(body);
+
+            const isUrl = UrlValidation.isUrl(result.expanded_url);
+
+            expect(isUrl).to.be.true;
+            done();
+        });
+    });
+});
